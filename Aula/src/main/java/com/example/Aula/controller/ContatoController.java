ed1ff541a0fc79f8e1fcf847b1bc9e4e03b4c5de
vilsonmoro <vilsonmoro@gmail.com>
2023-11-08 20:27:00 -0300

    Projeto criado

---
+++ Aula/src/main/java/com/example/Aula/controller/ContatoController.java
@@ -0,0 +1,25 @@
+package com.example.Aula.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContatoController {
+	
+	@GetMapping("/hello")
+    public String hello(@RequestParam(value = "name",
+     defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+	
+	@GetMapping("/contatos")
+	public String getContatos() {
+		return "lista de contatos";
+	}
+	
+	@GetMapping("/contatos/{id}")
+	public String getUmContato() {
+		return "Um contato";
+	}
+}
