d576a6089d6c7f2b496afc0f2af8563bd2404760
vilsonmoro <vilsonmoro@gmail.com>
2023-11-08 20:41:52 -0300

    Adicionado endpoint inserir contato

--- Aula/src/main/java/com/example/Aula/controller/ContatoController.java
+++ Aula/src/main/java/com/example/Aula/controller/ContatoController.java
@@ -1,17 +1,20 @@
 package com.example.Aula.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ContatoController {
 	
-	@GetMapping("/hello")
-    public String hello(@RequestParam(value = "name",
-     defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
-    }
+	@PostMapping("/contatos")
+	public ResponseEntity<String> inserirContato(@RequestBody String contato) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(contato);
+	}
 	
 	@GetMapping("/contatos")
 	public String getContatos() {
