eb1ffd75a6a57404c593ac29397739fb2cbd5d38
vilsonmoro <vilsonmoro@gmail.com>
2023-11-09 19:35:43 -0300

    Adicionado anotações JPA

--- Aula/src/main/java/com/example/Aula/entities/Contato.java
+++ Aula/src/main/java/com/example/Aula/entities/Contato.java
@@ -1,17 +1,26 @@
 package com.example.Aula.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
+@Table( name = "tb_contatos")
 public class Contato {
 	@Id
 	@GeneratedValue( strategy = GenerationType.IDENTITY)
 	private int id;
+	@Column(length = 50, nullable = false)
 	private String nome;
+	@Column(length = 100, nullable = false)
 	private String email;
+	
+	@Transient
+	private String observacao;
 
 	public int getId() {
 		return id;
@@ -35,6 +44,14 @@ public class Contato {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
 	}
 
 	public Contato(int id, String nome, String email) {
