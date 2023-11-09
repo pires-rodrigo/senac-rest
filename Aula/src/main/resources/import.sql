232db28d4e83e1e1a5f00ea96ad0808e42c634ce
vilsonmoro <vilsonmoro@gmail.com>
2023-11-09 20:40:58 -0300

    Adicionado arquivo para inserir dados

---
+++ Aula/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into tb_contatos(nome, email)values('maria','maria@gmail.com');
+insert into tb_contatos(nome, email)values('joao','joao@gmail.com');
+insert into tb_contatos(nome, email)values('pedro','pedro@gmail.com');
