f565bf3eee5ef528b0c22221615057a799e67ace
vilsonmoro <vilsonmoro@gmail.com>
2023-11-08 21:01:49 -0300

    Adicionado endpoint alterar contato

--- Aula/src/main/java/com/example/Aula/controller/ContatoController.java
+++ Aula/src/main/java/com/example/Aula/controller/ContatoController.java
@@ -4,24 +4,33 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/contatos")
 public class ContatoController {
 	
-	@PostMapping("/contatos")
+	@PostMapping()
 	public ResponseEntity<String> inserirContato(@RequestBody String contato) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(contato);
 	}
 	
-	@GetMapping("/contatos")
+	@PutMapping("/{idcontato}")
+	public ResponseEntity<String> alterarContato(@PathVariable("idcontato") int id, @RequestBody String contato) {
+		return ResponseEntity.status(HttpStatus.OK).body(contato);
+	}
+	
+	@GetMapping()
 	public String getContatos() {
 		return "lista de contatos";
 	}
 	
-	@GetMapping("/contatos/{id}")
+	@GetMapping("/{id}")
 	public String getUmContato() {
 		return "Um contato";
 	}
