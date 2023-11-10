d0800876f89bf6c20268b5704abc42205ccb3fa3
vilsonmoro <vilsonmoro@gmail.com>
2023-11-09 21:47:51 -0300

    Criar endpoint excluir

--- Aula/src/main/java/com/example/Aula/controller/ContatoController.java
+++ Aula/src/main/java/com/example/Aula/controller/ContatoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,4 +63,18 @@ public class ContatoController {
 		   return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
 	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Contato> excluirUmContato(@PathVariable("id") long id) {
+		Optional<Contato> opContato = repo.findById(id);
+		try {
+			Contato ct = opContato.get();	
+			repo.delete(ct);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}
+		catch(Exception e) {
+		   return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
+	
 }
