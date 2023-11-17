d1bdbf34c66cc044f2f6e41fc00e4c3e033f643e
vilsonmoro <vilsonmoro@gmail.com>
2023-11-16 21:43:09 -0300

    Add atributo transient

---
+++ Aula/src/main/java/com/example/Aula/entities/Contato.java
@@ -0,0 +1,67 @@
+package com.example.Aula.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+@Entity
+@Table( name = "tb_contatos")
+public class Contato {
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(length = 50, nullable = false)
+	private String nome;
+	@Column(length = 100, nullable = false)
+	private String email;
+	
+	@Transient
+	private String observacao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
+	public Contato(Long id, String nome, String email) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public Contato() {
+
+	}
+
+}
